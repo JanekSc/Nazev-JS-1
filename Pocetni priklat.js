40279ef3ed381753d4cde8e7c70faa2dcf086160
JanekSc <schafferj@spsstav.cz>
2023-03-27 10:01:34 +0200

    změna 27.3

--- Pocetni priklat.js
+++ Pocetni priklat.js
@@ -12,7 +12,22 @@ if (isNaN(polomer)) {
   let obsah = Math.PI * Math.pow(polomer, 2);
 
   // Vypiš výsledky na obrazovku
-  console.log("Poloměr kruhu je: " + polomer);
-  console.log("Obvod kruhu je: " + obvod);
-  console.log("Obsah kruhu je: " + obsah);
+  document.write("Poloměr kruhu je:" + polomer+ "<br>");
+  document.write("Obvod kruhu je: " + obvod+ "<br>");
+  document.write("Obsah kruhu je: " + obsah+"<br>");
 }
+
+function polomer1(polomer) { return obvod = 2 * Math.PI *polomer}
+
+function obvod(input) {
+    let converted = polomer1 (input.value);
+    document.getElementById("obvod").innerText = converted;
+  }
+
+  function inczk(polomer) { return obsah = Math.PI * Math.pow(polomer, 2)}
+
+function obsah(input) {
+    let converted = polomer1(input.value);
+    document.getElementById("obsah").innerText = converted;
+  }
+  
